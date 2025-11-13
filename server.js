20437bcf19ce6e862cb46e74c5635cd596ef6247
Sunil Paswan <aboharpunjab006@gmail.com>
2025-11-13 21:36:57 +0530

    initial backend commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// socket connection
+io.on("connection", (socket) => {
+  console.log("User connected: " + socket.id);
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
