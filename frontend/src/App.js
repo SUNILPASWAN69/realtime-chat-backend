7f036dfcdc58f2d9f1dcdca08607dec41cd2f80d
SUNIL <160910215+SUNILPASWAN69@users.noreply.github.com>
2025-11-14 15:53:33 +0530

    Change socket connection URL to production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://realtime-chat-backend-1.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
