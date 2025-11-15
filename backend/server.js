31b3287c94f98e2f34643f3a8181195c9d5d4923
SUNIL <160910215+SUNILPASWAN69@users.noreply.github.com>
2025-11-15 21:07:23 +0530

    Update CORS settings and log user joins
    Removed credentials option from CORS configuration and added console log for user joining.

--- backend/server.js
+++ backend/server.js
@@ -5,40 +5,36 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true
+    methods: ["GET", "POST"]
   }
 });
 
-// Store online users
 let onlineUsers = {};
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  // WHEN USER JOINS
   socket.on("join", (username) => {
+    console.log("User joined:", username);
+
     onlineUsers[socket.id] = username;
 
-    // Broadcast updated user list
     io.emit("online_users", Object.values(onlineUsers));
   });
-
-  // WHEN USER SENDS MESSAGE
   socket.on("send_message", (data) => {
     io.emit("receive_message", data);
   });
-
-  // WHEN USER DISCONNECTS
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
+
     delete onlineUsers[socket.id];
-    
+
     io.emit("online_users", Object.values(onlineUsers));
   });
 });
